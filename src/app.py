63c9e0c9395cbb7dc3bca42c14d43332373c34b4
Jorge Ortiz <Ecnavda@gmail.com>
2019-03-15 16:27:56 -0400

    Added static and template files for Flask to work with.

--- src/app.py
+++ src/app.py
@@ -1,13 +1,17 @@
 from pymongo import MongoClient
 import ron
+from flask import Flask, render_template
 import os
 APP_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_PATH = os.path.join(APP_PATH, "templates/")
 STATIC_PATH = os.path.join(APP_PATH, "static/")
 
-from flask import Flask, render_template
 # creating an instance of the Flask class using the special __name__ variable
-website = Flask(__name__, static_folder=STATIC_PATH,template_folder=TEMPLATE_PATH)
+website = Flask(
+        __name__,
+        static_folder=STATIC_PATH,
+        template_folder=TEMPLATE_PATH
+        )
 
 """
 # Connecting to the database
@@ -27,12 +31,12 @@ def index():
 
 @website.route("/quote")
 def quote():
-    return ron.get_quote()[0]
+    quotes = ron.get_quote()
+    return render_template("quotes.html", quotes=quotes)
 
 
 @website.route("/quote/<num>")
 def index_multi(num):
     # quotes is a list
-    x = ron.get_quotes(num)
-
-    return ron.get_quotes(num)
+    quotes = ron.get_quotes(num)
+    return render_template("quotes.html", quotes=quotes)
---
+++ static/main.js
@@ -0,0 +1,6 @@
+document.getElemebtById("quote_one").addEventListener("click",
+	function() {
+		document.getElementById("test").innerHTML = "Hello World!";
+	}
+);
+
